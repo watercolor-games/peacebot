431a344780660bcaba06769ad5238997f1c26d86
WatercolorGames <theplexgate@gmail.com>
2018-02-09 17:16:37 -0500

    Fix quote command

--- commands/quote.js
+++ commands/quote.js
@@ -9,15 +9,15 @@ exports.run = (client, message, args) => {
     const Discord = require('discord.js');
     let embed = new Discord.RichEmbed();
     if (!fs.existsSync('./quotes.json')) return message.channel.send("There is no database to grab information off of.");
-    fs.readFileSync('./quotes.json', (err, content) => {
-        if (err) console.error(err);
-        let quotesToChoose = JSON.parse(content);
-        let quoteIndexChosen = Math.floor(Math.random() * quotesToChoose.content.length);
-        embed.setTitle(`Quote of the Day! Message author: ${client.users.get(quotesToChoose.user[quoteIndexChosen]).user.tag}`)
-            .setDescription(quotesToChoose.content[quoteIndexChosen])
-            .setColor('GREEN')
-            .setThumbnail(client.users.get(quotesToChoose.user[quoteIndexChosen]).avatarURL)
-            .setFooter(`Message created on ${quotesToChoose.timestamp[quoteIndexChosen]} | Message ID: ${quotesToChoose.id[quoteIndexChosen]}`)
-        message.channel.send({ embed });
-    });
+    let json = fs.readFileSync('./quotes.json');
+    let quotesToChoose = JSON.parse(json);
+    let quoteIndexChosen = Math.floor(Math.random() * quotesToChoose.length);
+    let quote = quotesToChoose[quoteIndexChosen];
+    let quoteUser = client.users.get(quote.user);
+    embed.setTitle(`Here's your quote! Message author: ${quoteUser.tag}`)
+        .setDescription(quote.content)
+        .setColor('GREEN')
+        .setThumbnail(quoteUser.avatarURL)
+        .setFooter(`Message created on ${quote.timestamp} | Message ID: ${quote.id}`)
+    message.channel.send({ embed });
 };
